b0859caf7fca8a6ea74e3931e84c92d878b8ea50
fdevmx <filesforcloud.fdev.mx@gmail.com>
2024-11-11 03:32:34 -0600

    update methods

--- backend/app.py
+++ backend/app.py
@@ -1,26 +1,30 @@
+# app.py
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from cli import lexer, parser, execute_command
+from cli import process_command
 
 app = Flask(__name__)
-CORS(app)  # Permite CORS para todas las rutas
+CORS(app)
 
 @app.route('/execute', methods=['POST'])
 def execute():
-    data = request.get_json()
-    command = data.get('command', '')
-    
-    # Lexical analysis
-    lexer.input(command)
-    
-    # Syntactic analysis
-    parsed_command = parser.parse(command)
-    
-    if parsed_command:
-        output = execute_command(parsed_command)
-        return jsonify({'output': output}), 200
-    else:
-        return jsonify({'error': 'Invalid command'}), 400
+    try:
+        data = request.get_json()
+        command = data.get('command', '')
+        
+        if not command:
+            return jsonify({'error': 'No command provided'}), 400
+        
+        # Procesar el comando usando la nueva función integrada
+        result = process_command(command)
+        
+        return jsonify({
+            'lexical_analysis': result['lexical_analysis'],
+            'execution_result': result['execution_result']
+        }), 200
+        
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
