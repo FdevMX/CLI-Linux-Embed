46bc4389c5d45458d175ba06250e9fed5670b2c5
fdevmx <filesforcloud.fdev.mx@gmail.com>
2024-11-11 00:01:13 -0600

    configure routes

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    async rewrites() {
+      return [
+        {
+          source: '/execute',
+          // destination: 'https://analizador-sintactico-back.onrender.com/analyze', // Ajusta esta URL
+          destination: 'http://localhost:5000/execute', // Asume que Flask está corriendo en el puerto 5000
+          // destination: '/analyze', // Esto sigue apuntando a la ruta del backend
+        },
+      ]
+    },
+    // Asegúrate de que el output sea 'standalone' para Vercel
+    // output: 'standalone',
+}
+  
+  export default nextConfig;
